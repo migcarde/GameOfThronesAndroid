52d12c3be64b723a4ef3c3ac0475b806994e7bda
Miguel Caraballo de la Rosa <mcaraballodelarosa@outlook.com>
2021-04-02 14:11:28 +0200

    Categories list #3
    - Added tests for view model.

---
+++ presentation/src/test/java/com/example/gameofthrones/CategoriesViewModelTest.kt
@@ -0,0 +1,101 @@
+package com.example.gameofthrones
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.example.gameofthrones.operations.categories.CategoriesViewModel
+import com.example.gameofthrones.operations.categories.CategoriesViewState
+import com.example.gameofthrones.operations.categories.CategoriesViewTransition
+import com.example.gameofthrones.operations.categories.CategoryViewEntity
+import com.example.gameofthrones.operations.splash.SplashViewEntity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.setMain
+import org.junit.*
+import org.mockito.ArgumentCaptor
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+
+class CategoriesViewModelTest {
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    // View model parameters
+    private val state = Mockito.mock(Observer::class.java as Class<Observer<CategoriesViewState>>)
+    private val transition =
+        Mockito.mock(Observer::class.java as Class<Observer<CategoriesViewTransition>>)
+
+    private val viewModel = CategoriesViewModel()
+
+    // Example values for testing
+    val splashViewEntity = listOf(
+        SplashViewEntity(
+            categoryName = "Test",
+            type = 0
+        )
+    )
+
+    val expectedResult = listOf(
+        CategoryViewEntity(
+            categoryName = "Test",
+            type = 0
+        )
+    )
+
+    val TYPE_BOOKS = 0
+
+    @ExperimentalCoroutinesApi
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        Dispatchers.setMain(Dispatchers.Default)
+    }
+
+    @After
+    fun tearDown() {
+        Mockito.framework().clearInlineMocks()
+    }
+
+    @Test
+    fun `Get categories - Success`() = runBlocking {
+        // Given
+        viewModel.getState().observeForever(state)
+
+        val argumentCaptor = ArgumentCaptor.forClass(CategoriesViewState::class.java)
+
+        // When
+        viewModel.setCategories(splashViewEntity)
+
+        // Then
+        Mockito.verify(state).onChanged(argumentCaptor.capture())
+
+        val currentState = argumentCaptor.allValues[0]
+
+        Assert.assertEquals(
+            CategoriesViewState.Categories(data = expectedResult),
+            currentState
+        )
+    }
+
+    @Test
+    fun `Get categories - Go to books`() = runBlocking {
+        // Given
+        viewModel.getTransition().observeForever(transition)
+
+        val argumentCaptor = ArgumentCaptor.forClass(CategoriesViewTransition.GoToBooks(TYPE_BOOKS)::class.java)
+
+        // When
+        viewModel.goTo(TYPE_BOOKS)
+
+        // Then
+        Mockito.verify(transition).onChanged(argumentCaptor.capture())
+
+        val currentState = argumentCaptor.allValues[0]
+
+        Assert.assertEquals(
+            CategoriesViewTransition.GoToBooks(TYPE_BOOKS),
+            currentState
+        )
+    }
+}
